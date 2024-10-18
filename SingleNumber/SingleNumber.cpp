4ea3a16d8dce8d96856f5dab9fb8fd627a5a6dd4
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-18 10:26:43 +0530

    Solution-GeeksforGeeks

---
+++ SingleNumber/SingleNumber.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    int getSingle(vector<int>& arr) {
+        int xora = 0, n = arr.size();
+        for (int i = 0; i < n; i++) {
+            xora = xora ^ arr[i];
+        }
+        return xora;
+    }
+};
+
+    
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.getSingle(arr);
+        cout << res << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
