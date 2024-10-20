a9d544f6a8040637b4cadbd141062d27920fe0b7
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-20 16:31:09 +0530

    solution-GfG

---
+++ Difficulty Level - Easy/Sort a k sorted doubly linked list/SortaKsortedDoublyLinkedList.cpp
@@ -0,0 +1,146 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// a node of the doubly linked list
+class DLLNode {
+  public:
+    int data;
+    DLLNode *next;
+    DLLNode *prev;
+
+    DLLNode(int val) {
+        data = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+
+struct compare {
+    bool operator()(DLLNode* a, DLLNode* b) {
+        return a->data > b->data;
+    }
+};
+class Solution {
+public:
+    // Function to sort a k-sorted doubly linked list
+    DLLNode *sortAKSortedDLL(DLLNode *head, int k) {
+        if (head == NULL) return head;
+
+        // Min-heap to store the k+1 elements
+        priority_queue<DLLNode*, vector<DLLNode*>, compare> minHeap;
+
+        DLLNode *newHead = NULL, *last = NULL;
+
+        // Push the first k+1 elements into the min-heap
+        for (int i = 0; head && i <= k; i++) {
+            minHeap.push(head);
+            head = head->next;
+        }
+
+        // Process the remaining elements in the linked list
+        while (!minHeap.empty()) {
+            // Extract the minimum element from the heap
+            DLLNode *minNode = minHeap.top();
+            minHeap.pop();
+
+            // If this is the first node, set it as new head
+            if (newHead == NULL) {
+                newHead = minNode;
+                newHead->prev = NULL;
+                last = newHead;
+            } else {
+                last->next = minNode;
+                minNode->prev = last;
+                last = minNode;
+            }
+
+            // If more elements exist in the original list, push them into the heap
+            if (head) {
+                minHeap.push(head);
+                head = head->next;
+            }
+        }
+
+        // Mark the end of the sorted doubly linked list
+        last->next = NULL;
+
+        return newHead;
+    }
+};
+        
+//{ Driver Code Starts.
+
+// Function to insert a node at the end
+// of the Doubly Linked List
+void push(DLLNode **tailRef, int new_data) {
+    // allocate node
+    DLLNode *newNode = new DLLNode(new_data);
+
+    // since we are adding at the end,
+    // next is NULL
+    newNode->next = NULL;
+
+    newNode->prev = (*tailRef);
+
+    // change next of tail node to new node
+    if ((*tailRef) != NULL)
+        (*tailRef)->next = newNode;
+
+    (*tailRef) = newNode;
+}
+
+// Function to print nodes in a given doubly linked list
+void printList(DLLNode *head) {
+    // if list is empty
+    if (head == NULL)
+        return;
+
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+// Driver
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int k;
+        cin >> k;
+        cin.ignore();
+
+        DLLNode *head = new DLLNode(arr[0]);
+        DLLNode *tail = head;
+
+        // Check if the array is empty
+        for (size_t i = 1; i < arr.size(); ++i) {
+            push(&tail, arr[i]);
+        }
+        Solution obj;
+        printList(obj.sortAKSortedDLL(head, k));
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
