45ce7042f380bebb2835e3aad93297259757fc2b
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-25 13:11:44 +0530

    Solution -GfG

--- Difficulty Level - Easy/DeleteAlternateCode/DeleteAlternateCode.cpp
+++ Difficulty Level - Easy/DeleteAlternateCode/DeleteAlternateCode.cpp
@@ -34,8 +34,6 @@ void append(struct Node **head_ref, int new_data) {
     }
     while (last->next != NULL)
         last = last->next;
-
-    /* 6. Change the next of last node */
     last->next = new_node;
     return;
 }
