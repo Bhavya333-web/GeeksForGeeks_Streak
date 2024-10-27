da45f8b522891e239d150abd401d3b4fe8195cbb
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-28 03:02:16 +0530

    Solution - GfG

--- Difficulty Level - Easy/Remove Duplicates in array/Remove Duplicates in array.cpp
+++ Difficulty Level - Easy/Remove Duplicates in array/Remove Duplicates in array.cpp
@@ -3,7 +3,6 @@ using namespace std;
 class Solution {
   public:
     vector<int> removeDuplicate(vector<int>& arr) {
-        // code here
         for(int i=arr.size()-1;i>=0;i--){
             int k=arr[i];
             arr[i]=0;
