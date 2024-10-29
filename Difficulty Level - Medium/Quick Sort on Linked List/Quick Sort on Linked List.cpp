4f6a99e87bfb869bd0f77b1d27751a406bc256e0
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-29 22:23:34 +0530

    Solution - GfG

--- Difficulty Level - Medium/Quick Sort on Linked List/Quick Sort on Linked List.cpp
+++ Difficulty Level - Medium/Quick Sort on Linked List/Quick Sort on Linked List.cpp
@@ -14,13 +14,8 @@ struct Node {
     }
 };
 void push(struct Node*& head_ref, int new_data) {
-    /* allocate node */
     struct Node* new_node = new Node(new_data);
-
-    /* link the old list off the new node */
     new_node->next = head_ref;
-
-    /* move the head to point to the new node */
     head_ref = new_node;
 }
 
