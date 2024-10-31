48618e671a07f1d157bd64fb43b634d6bf80a236
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-11-01 01:30:45 +0530

    Solution - GfG

--- Difficulty Level - Easy/Balanced Tree Check/Balanced Tree Check.cpp
+++ Difficulty Level - Easy/Balanced Tree Check/Balanced Tree Check.cpp
@@ -13,9 +13,6 @@ Node* newNode(int val) {
 
     return temp;
 }
-
-
-// Function to Build Tree
 Node* buildTree(string str) {
     // Corner Case
     if (str.length() == 0 || str[0] == 'N') return NULL;
