902d79fff6c3dd55afbda76b9c8893dbe935f8e9
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-11-11 13:31:02 +0530

    GfG-Solutions

--- Difficulty Level - Medium/Make array elements unique/Make_array_elements_unique.java
+++ Difficulty Level - Medium/Make array elements unique/Make_array_elements_unique.java
@@ -1,12 +1,10 @@
 import java.io.*;
 import java.lang.*;
 import java.util.*;
-
 class GFG {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-
         while (t-- > 0) {
             String arr[] = br.readLine().split(" ");
             int a[] = new int[arr.length];
