3b0ad3602b1986bbc896e0a530fe6f736429324a
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-11-14 13:18:15 +0530

    GfG-Solutions

--- Difficulty Level - Medium/Nearly sorted/Nearly_sorted.cpp
+++ Difficulty Level - Medium/Nearly sorted/Nearly_sorted.cpp
@@ -4,8 +4,6 @@ class Solution {
  public:
     void nearlySorted(vector<int>& arr, int k) {
         int n = arr.size();
-        
-        // make a min heap
         priority_queue<int,vector<int>,greater<int>> pq;
         for(int i=0;i<=k;++i)
             pq.push(arr[i]);
