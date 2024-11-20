7a99d1fb82486deb5a0f9e97d26a908d95135c9c
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-11-20 11:49:40 +0530

    GfG-Solutions

--- Difficulty Level - Medium/Majority Element II/Majority_Element_II.cpp
+++ Difficulty Level - Medium/Majority Element II/Majority_Element_II.cpp
@@ -6,19 +6,28 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    // Function to find the majority elements in the array
-    vector<int> findMajority(vector<int>& nums) {
-        vector<int>ans;
-        unordered_map<int,int>mp;
-        for(auto &it:nums){
-            mp[it]++;
+   vector<int> findMajority(vector<int>& arr) {
+        vector<int> ans; 
+        int n = arr.size(); 
+        sort(arr.begin(),arr.end()); 
+        int count =1; 
+        int num; 
+        for(int i =0;i<n-1;i++){
+            num = arr[i];
+            if(arr[i]==arr[i+1]){
+                count++; 
+            }
+            else{
+                if(count > n/3 ){
+                    ans.push_back(num); 
+                }
+                count = 1; 
+            }
         }
-        for(auto it:mp){
-            if(it.second>(nums.size()/3)) ans.push_back(it.first);
+        if (count > n / 3) {
+            ans.push_back(arr[n-1]);
         }
-        sort(ans.begin(),ans.end());
-        if(ans.size()==0) return {-1};
-        return ans;
+        return ans ; 
     }
 
 };
