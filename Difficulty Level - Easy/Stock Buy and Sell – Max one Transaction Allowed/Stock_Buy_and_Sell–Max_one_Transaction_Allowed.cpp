dc59378579eb212f5a574951fbb6e3ce9e15d68e
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-11-22 15:11:42 +0530

    GfG-Solutions

--- Difficulty Level - Easy/Stock Buy and Sell – Max one Transaction Allowed/Stock_Buy_and_Sell–Max_one_Transaction_Allowed.cpp
+++ Difficulty Level - Easy/Stock Buy and Sell – Max one Transaction Allowed/Stock_Buy_and_Sell–Max_one_Transaction_Allowed.cpp
@@ -3,8 +3,6 @@ using namespace std;
 class Solution {
   public:
     int maximumProfit(vector<int> &prices) {
-        // code here
-        // Time Complexity O(n)  | Space Complexity O(1)
         int maxProfit = 0; // result
         int mbp = prices[0]; // store the minimum buying price
         for(int i =1;i<prices.size();i++){
