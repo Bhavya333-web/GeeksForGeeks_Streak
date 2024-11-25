0ed0bcdb8d680a48494b259d5ed1310cb25ccf72
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-11-25 14:54:23 +0530

    GfG-Solutions

--- Difficulty Level - Medium/Maximum Product Subarray/Maximum_Product_Subarray.cpp
+++ Difficulty Level - Medium/Maximum Product Subarray/Maximum_Product_Subarray.cpp
@@ -32,14 +32,7 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        // int n, i;
-        // cin >> n;
-        // vector<int> arr(n);
-        // for (i = 0; i < n; i++) {
-        //     cin >> arr[i];
-        // }
-
-        vector<int> arr;
+             vector<int> arr;
         string input;
 
         // Read array
