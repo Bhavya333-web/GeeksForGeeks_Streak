a5060b5823081d331dbe626d6d7676b2ba01f842
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-11-30 11:24:29 +0530

    GfG-Solutin

--- Difficulty Level - Easy/Anagram/Anagram.cpp
+++ Difficulty Level - Easy/Anagram/Anagram.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 class Solution {
   public:
-    // Function is to check whether two strings are anagram of each other or not.
      bool areAnagrams(string& s1, string& s2) {
         // Your code here
         sort(s1.begin(),s1.end());
