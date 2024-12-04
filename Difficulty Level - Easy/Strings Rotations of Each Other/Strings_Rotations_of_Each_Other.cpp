e866447be3178e672f33edfa8dc4c501908d086a
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-12-04 21:30:00 +0530

    GfG-Solutin

--- Difficulty Level - Easy/Strings Rotations of Each Other/Strings_Rotations_of_Each_Other.cpp
+++ Difficulty Level - Easy/Strings Rotations of Each Other/Strings_Rotations_of_Each_Other.cpp
@@ -3,7 +3,6 @@ using namespace std;
 class Solution {
   public:
     bool areRotations(string &s1, string &s2) {
-        // Your code here
         if(s1.size()!=s2.size()) return false;
         s1=s1+s1;
         return s1.find(s2)!=string::npos;
