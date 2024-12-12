0d35b12e8cace11955bfd9227fab68f29301433e
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-12-12 12:52:26 +0530

    GfG-Solutin

---
+++ Difficulty Level - Easy/Number of occurrence/Number_of_occurrence.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    // Function to count the number of occurrences of target
+    int countFreq(vector<int>& arr, int target) {
+        int lo = lastOccurrence(arr, target);
+        int fo = firstOccurrence(arr, target);
+        if (lo == -1 && fo == -1) return 0;
+        return (lo - fo) + 1;
+    }
+
+private:
+    // Method to find the first occurrence of the target element
+    int firstOccurrence(vector<int>& arr, int target) {
+        int start = 0, end = arr.size() - 1, fo = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                fo = mid;
+                end = mid - 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return fo;
+    }
+
+    // Method to find the last occurrence of the target element
+    int lastOccurrence(vector<int>& arr, int target) {
+        int start = 0, end = arr.size() - 1, lo = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                lo = mid;
+                start = mid + 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return lo;
+    }
+};
+
+// Driver code
+int main() {
+    int tc;
+    cin >> tc; // Number of test cases
+    while (tc-- > 0) {
+        // Reading array input
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> arr[i];
+        }
+
+        // Reading target
+        int k;
+        cin >> k;
+
+        // Solution class instance
+        Solution sln;
+        int ans = sln.countFreq(arr, k);
+
+        // Output result
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
